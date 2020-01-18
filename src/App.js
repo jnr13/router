d3faa7d048f77c39b590b9a928db3ca97b347c49
alex.dana@epitech.eu <alex.dana@epitech.eu>
2020-01-18 11:32:08 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import { About, User } from "./routes/";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/user/alex">Alex user</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route path="/user/:name">
+            <User />
+          </Route>
+          <Route path="/users">Users</Route>
+          <Route exact path="/">
+            Home
+          </Route>
+          <Route path="*">No content 404</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
